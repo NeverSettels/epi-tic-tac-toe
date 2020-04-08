ea4964028e54e2a8d9b5cc1ae3d3c8fe7454c67a
NeverSettels <settels.eric@gmail.com>
2020-04-08 13:56:02 -0700

    add UI for displaying winners and losers and ties and wrong

--- js/scripts.js
+++ js/scripts.js
@@ -13,28 +13,20 @@ function Board() {
 
 Board.prototype.checkWinner = function () {
   if (this.tl && this.tl === this.tm && this.tm === this.tr) {
-    console.log(`${this.tl} winns!`);
     this.over = true
   } else if (this.ml && this.ml === this.mm && this.mm === this.mr) {
-    console.log(`${this.ml} winns!`);
     this.over = true
   } else if (this.bl && this.bl === this.bm && this.bm === this.br) {
-    console.log(`${this.bl} winns!`);
     this.over = true
   } else if (this.tl && this.tl === this.ml && this.ml === this.bl) {
-    console.log(`${this.tl} winns!`);
     this.over = true
   } else if (this.tr && this.tr === this.mr && this.mr === this.br) {
-    console.log(`${this.tr} winns!`);
     this.over = true
   } else if (this.tm && this.tm === this.mm && this.mm === this.bm) {
-    console.log(`${this.tm} winns!`);
     this.over = true
   } else if (this.tl && this.tl === this.mm && this.mm === this.br) {
-    console.log(`${this.tl} winns!`);
     this.over = true
   } else if (this.tr && this.tr === this.mm && this.mm === this.bl) {
-    console.log(`${this.tr} winns!`);
     this.over = true
   } else {
     this.over = false
@@ -46,7 +38,7 @@ $(document).ready(() => {
   let game = new Board()
   let turn = 1;
   const plop = new Audio('https://freesound.org/data/previews/19/19987_37876-lq.mp3')
-  console.log(null == null)
+  const wrong = new Audio('https://freesound.org/data/previews/325/325443_4490625-lq.mp3')
   $('#board-container').on('click', 'div', event => {
     let key = event.target.id
     if (!game[key]) {
@@ -62,14 +54,18 @@ $(document).ready(() => {
       if (game.over || turn >= 9) {
         $("#board-container div").prop("disabled", true);
         if (!game.over) {
-          console.log('its a tie');
+          $("#message").html(`<h1 class="animated flipInX blue">It's a tie both you suck</h1>`)
+        } else {
+          $("#message").html(`<h1 class="animated flip green">${game[key]} is the Winner</h1>`)
         }
+
       }
       turn++
     } else {
-      console.log('oops no valid');
+      plop.pause()
+      wrong.play()
+      $('#message').html(`<h1 class="animated hinge red"> WRONG</h1>`);
     }
-    console.log(game)
   })
 })
 
